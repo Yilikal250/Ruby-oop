7f3b07233a33ddd93e3053ed35a2aae22d81d3c4
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2023-01-05 09:08:23 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,6 +1,6 @@
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission, classroom)
+    super(name, age, parent_permission)
     @classroom = classroom
   end
 
