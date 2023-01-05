ca8ad49ce4b3b060bfd66c3270d486cbad7303e6
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2023-01-05 09:09:10 +0300

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     @specialization = specialization
-    super(name, age, parent_permission, classroom, specialization)
+    super(name, age, parent_permission)
   end
 
   def can_use_services?
