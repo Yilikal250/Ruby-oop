1a9ffd3e3e9ba76d65afc469a9496d192bb93062
Yilikal250 <teshomeyilikal250@gmail.com>
2023-01-07 13:09:07 +0300

    conflict resolved

--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,12 @@
 require_relative './person'
-# Class: Teacher inherits from Person
+
 class Teacher < Person
-  # initialize method adds a new instance variable @specialization
+ 
   def initialize(*person, specialization)
     super(*person)
     @specialization = specialization
   end
 
-  # method to always return true in can_use_services?
   def can_use_services?
     true
   end
